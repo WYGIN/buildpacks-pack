af5d72cd8b898e2cf8736b34014325e2019df80b
Zymate Studio <sai.pubg.blog@gmail.com>
2023-11-01 15:11:14 +0000

    added client logic for manifest cli

--- pkg/client/push_manifest.go
+++ pkg/client/push_manifest.go
@@ -1,6 +1,32 @@
 package client
 
-// PushManifest implements commands.PackClient.
-func (*Client) PushManifest() error {
-	panic("unimplemented")
+import (
+	"context"
+
+	// "github.com/buildpacks/imgutil"
+)
+
+type PushManifestOptions struct {
+	Format string
+	Insecure, Purge bool
 }
+// PushManifest implements commands.PackClient.
+func (c *Client) PushManifest(ctx context.Context, index string, opts PushManifestOptions) (imageID string, err error) {
+	// img, err := c.indexFactory.NewIndex(index, parseFalgsForImgUtil(opts))
+
+	// if err != nil {
+	// 	return err
+	// }
+
+	// if opts.Purge {
+	// 	if err := img.Delete(); err != nil {
+	// 		return err
+	// 	}
+	// }
+
+	return imageID, err
+}
+
+// func parseFalgsForImgUtil(opts PushManifestOptions) (idxOptions imgutil.IndexOptions) {
+// 	return idxOptions
+// }
